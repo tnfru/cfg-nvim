6060efeceeb2f268a2c8627f6f890ece5dae761c
Lars Mueller <lars.lmueller@gmail.com>
2025-03-19 14:50:54 +0100

    add keybinds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,21 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("i", "jk", "<ESC>", { desc = "Exit insert mode by pressing jk quickly"})
+
+map("n", "x", '"_x', { desc = "Dont copy char deleted by x"})
+--
+-- save and quit using ctrl
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-q>", "<cmd> q <cr>")
+
+-- scroll
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- resize buffer
+map("n", "<Up>", ":resize -2<CR>")
+map("n", "<Down>", ":resize +2<CR>")
+map("n", "<Left>", ":vertical resize -2<CR>")
+map("n", "<Right>", ":vertical resize +2<CR>")
