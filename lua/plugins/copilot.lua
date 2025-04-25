b625d270048dd1df5419462cdd8edfeb672933a0
Lars Mueller <lars.lmueller@gmail.com>
2025-04-25 14:40:18 +0200

    disable copilot and avante

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("copilot").setup {
       panel = {
-        enabled = true,
+        enabled = false,
         auto_refresh = true,
         keymap = {
           jump_prev = "[[",
